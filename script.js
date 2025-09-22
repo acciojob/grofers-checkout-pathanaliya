751b229e832ced65a3ac681fcbcab21c63795228
Pathan Aliya <144582876+pathanaliya@users.noreply.github.com>
2025-09-22 10:19:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let prices= element.querySelectorAll("price");
+	let total = 0;
+	price.forEach((prices) =>{
+		total += parseInt(price.textContent); 
+	})
+
+	let table = document.querySelector("table");
+	let newRow = document.createElement("tr");
+	let newCell = document.createElement("td");
+
+	newCell.setAttribute("colspan" , "2");
+	newCell.style.fontWeight = "bold";
+	newCell.textContent = "Total Price: Rs" + total;
+
+	newRow.appendChild(newCell);
+	table.appendChild(newRow);
   
 };
 
